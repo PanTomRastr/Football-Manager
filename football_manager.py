a429074ec13273f52701c6a3036232779d1a9b34
Ri7ay <43480682+Ri7ay@users.noreply.github.com>
2018-12-07 21:22:34 +0300

    )))

--- football_manager.py
+++ football_manager.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
-from function import football_box, proverka, chek, sait
+from function import football_box, proverka, chek, sait, music, open_game_now, open_game_club
 import requests, bs4
 import playsound
+import threading
+
+t = threading.Thread(target=music, name='Thread1')
 
 class Main(tk.Frame):
     def __init__(self, root):
@@ -16,25 +19,30 @@ class Main(tk.Frame):
         toolbar.pack(side=tk.TOP,fill=tk.X)
 
 
-        self.add_img = tk.PhotoImage(file="add.gif")
+        self.add_img = tk.PhotoImage(file="soccer-game.png")
+        self.add_img2 = tk.PhotoImage(file="football.png")
+        self.add_img3 = tk.PhotoImage(file="schedule.png")
+        self.add_img4 = tk.PhotoImage(file="strategy.png")
+        self.add_img5 = tk.PhotoImage(file="music-player.png")
+
         btn_open_dialog = tk.Button(toolbar, text='Сравните команды', command=self.open_dialog, bg='#d7d8e0', bd=0,
                                     compound=tk.TOP, image=self.add_img)
         btn_open_dialog.pack(side=tk.LEFT)
 
         btn_open_game = tk.Button(toolbar, text='Последние игры 2-х комманд', command=self.open_game, bg='#d7d8e0',
-                                  bd=0, compound=tk.TOP, image=self.add_img)
+                                  bd=0, compound=tk.TOP, image=self.add_img2)
         btn_open_game.pack(side=tk.LEFT)
 
-        btn_open_game_now = tk.Button(toolbar, text='Последние игры клубов', command=self.open_game_now, bg='#d7d8e0',
-                                  bd=0, compound=tk.TOP, image=self.add_img)
+        btn_open_game_now = tk.Button(toolbar, text='Последние игры клубов', command=self.open_year_club, bg='#d7d8e0',
+                                  bd=0, compound=tk.TOP, image=self.add_img3)
         btn_open_game_now.pack(side=tk.LEFT)
 
-        btn_open_club = tk.Button(toolbar, text='Игры между странами', command=self.open_game_club, bg='#d7d8e0',
-                                      bd=0, compound=tk.TOP, image=self.add_img)
+        btn_open_club = tk.Button(toolbar, text='Игры между странами', command=self.open_year, bg='#d7d8e0',
+                                      bd=0, compound=tk.TOP, image=self.add_img4)
         btn_open_club.pack(side=tk.LEFT)
 
         btn_open_music = tk.Button(toolbar, text='музыка', command=self.open_music, bg='#d7d8e0',
-                                  bd=0, compound=tk.TOP, image=self.add_img)
+                                  bd=0, compound=tk.TOP, image=self.add_img5)
         btn_open_music.pack(side=tk.LEFT)
         ''''
         self.tree1 = ttk.Treeview(self, columns=('ID'))
@@ -65,6 +73,12 @@ class Main(tk.Frame):
 
     def open_game(self):
         Game(self)
+
+    def open_year(self):
+        Year(self)
+
+    def open_year_club(self):
+        Year_club(self)
 
     def print_text(self):
         football_teams = self.text.split()
@@ -111,17 +125,17 @@ class Main(tk.Frame):
             self.tree2.set('item3', 'ID', global1[1][1][2])
             self.tree2.set('item4', 'ID', global1[1][1][3])
 
-    def open_game_now(self):
-        s = requests.get('http://wildstat.ru/p/5')
-        b = bs4.BeautifulSoup(s.text, "html.parser")
-        global1 = b.select('.box')
-        Game_now(self, var=global1)
+    #def open_game_now(self):
+        #s = requests.get('http://wildstat.ru/p/5')
+        #b = bs4.BeautifulSoup(s.text, "html.parser")
+        #global1 = b.select('.box')
+        #Game_now(self, var=global1)
 
-    def open_game_club(self):
-        s = requests.get('http://wildstat.ru/p/6')
-        b = bs4.BeautifulSoup(s.text, "html.parser")
-        global1 = b.select('.tab-row-green')
-        Game_club(self, var=global1)
+    #def open_game_club(self):
+        #s = requests.get('http://wildstat.ru/p/6')
+        #b = bs4.BeautifulSoup(s.text, "html.parser")
+        #global1 = b.select('.tab-row-green')
+        #Game_club(self, var=global1)
 
     def print_game(self):
         football_teams = self.text.split()
@@ -134,8 +148,108 @@ class Main(tk.Frame):
             global1 = sait(football_teams[0], football_teams[1])
             show_Game(self, var=global1)
 
+    def print_year(self):
+        football_year = self.text
+        if football_year == '2019':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2019/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2018':
+            global1 = open_game_club('http://wildstat.ru/p/6')
+            Game_club(self, var=global1)
+
+        if football_year == '2017':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2017/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2016':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2016/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2015':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2015/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2014':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2014/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2013':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2013/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2012':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2012/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2011':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2011/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2010':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2010/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2009':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2009/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+        if football_year == '2008':
+            global1 = open_game_club('http://wildstat.ru/p/60/ch/WRL_FR_2008/stg/all/tour/all')
+            Game_club(self, var=global1)
+
+    def print_year_club(self):
+        football_year = self.text
+        if football_year == '2019/2018':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2018_2019/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2018/2017':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2017_2018/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2017/2016':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2016_2017/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2016/2015':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2015_2016/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2015/2014':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2014_2015/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2014/2013':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2013_2014/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2013/2012':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2012_2013/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2012/2011':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2011_2012/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2011/2010':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2010_2011/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2010/2009':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2009_2010/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2009/2008':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2008_2009/stg/all/tour/all')
+            Game_now(self, var=global1)
+
+        if football_year == '2008/2007':
+            global1 = open_game_now('http://wildstat.ru/p/50/ch/EUR_CL_2007_2008/stg/all/tour/all')
+            Game_now(self, var=global1)
+
     def open_music(self):
-        playsound.playsound('file2.mp3', True)
+        t.start()
 
 class Child(tk.Toplevel):
     def __init__(self, root):
@@ -269,6 +383,74 @@ class Game_club(tk.Toplevel):
         self.grab_set()
         self.focus_set()
 
+class Year(tk.Toplevel):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_year()
+        self.root = root
+
+    def init_year(self):
+        self.title('Футбольные Команды и их иглы')
+        self.geometry('400x220+400+300')
+        self.resizable(False, False)
+
+        label_select = tk.Label(self, text='Годы:')
+        label_select.place(x=50, y=80)
+
+        self.combobox = ttk.Combobox(self, values=[u"2019", u"2018", "2017", u"2016", u"2015", u"2014", "2013",
+                                                   u"2012", u"2011", u"2010", "2009", u"2008"])
+        self.combobox.current(0)
+        self.combobox.place(x=200, y=80)
+
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        btn_cancel.place(x=300, y=170)
+
+        btn_ok = ttk.Button(self, text='Посмотреть игры', command = self.add_year)
+        btn_ok.place(x=190, y=170)
+
+        self.grab_set()
+        self.focus_set()
+
+    def add_year(self):
+        self.root.text = self.combobox.get()
+        self.root.print_year()
+        self.destroy()
+
+class Year_club(tk.Toplevel):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_year()
+        self.root = root
+
+    def init_year(self):
+        self.title('Футбольные Команды и их иглы')
+        self.geometry('400x220+400+300')
+        self.resizable(False, False)
+
+        label_select = tk.Label(self, text='Годы:')
+        label_select.place(x=50, y=80)
+
+        self.combobox = ttk.Combobox(self, values=[u"2019/2018", u"2018/2017", "2017/2016", u"2016/2015", u"2015/2014",
+                                                   u"2014/2013", "2013/2012",
+                                                   u"2012/2011", u"2011/2010", u"2010/2009", "2009/2008",
+                                                   u"2008/2007"])
+        self.combobox.current(0)
+        self.combobox.place(x=200, y=80)
+
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        btn_cancel.place(x=300, y=170)
+
+        btn_ok = ttk.Button(self, text='Посмотреть игры', command = self.add_year)
+        btn_ok.place(x=190, y=170)
+
+        self.grab_set()
+        self.focus_set()
+
+    def add_year(self):
+        self.root.text = self.combobox.get()
+        self.root.print_year_club()
+        self.destroy()
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = Main(root)
@@ -277,3 +459,4 @@ if __name__ == "__main__":
     root.geometry("1280x720+300+200")
     root.resizable(False, False)
     root.mainloop()
+
--- function.py
+++ function.py
@@ -1,8 +1,9 @@
 import requests, bs4
 import re
 from PIL import ImageTk
+import playsound
 
-lst = ['авангард', 'уфа', 'анжи', 'ангушт', 'мордовия', 'cпартак-владикавказ', 'рубин', 'камаз', 'нефтехимик', 'ахмат',
+lst = ['авангард', 'уфа', 'анжи', 'ангушт', 'мордовия', 'cпартаквладикавказ', 'рубин', 'камаз', 'нефтехимик', 'ахмат',
        'динамо', 'краснодар', 'сочи', 'черноморец', 'енисей', 'звезда', 'ска-хабаровск', 'салют',
        'торпедо', 'муром', 'ротор-волгоград', 'ротор', 'Факел', 'балтика', 'калуга', 'ленинградец',
        'металлург', 'химки', 'коломна', 'знамя Труда', 'долгопрудный', 'сатурн', 'химки-м', 'нижний новгород',
@@ -160,17 +161,34 @@ def sait(lst1, lst2):
 
     if (lst1 == 'зенит' and lst2 == 'балтика') or (lst1 == 'балтика' and lst2 == 'зенит'):
         return  football_box('http://wildstat.ru/p/7001/ch/all/club1/RUS_Zenit_St_Petersburg/club2/RUS_Baltika_Kaliningrad')
+
+    if (lst1 == 'салют' and lst2 == 'cпартаквладикавказ') or (lst1 == 'салют' and lst2 == 'cпартаквладикавказ'):
+        return  football_box('http://wildstat.ru/p/7001/ch/all/club1/RUS_FC_Spartak_Vladikavkaz/club2/RUS_Salyut_Belgorod')
+
     return 'Нету комманд'
 
 
+def open_game_now(year):
+    s = requests.get(year)
+    b = bs4.BeautifulSoup(s.text, "html.parser")
+    global1 = b.select('.box')
+    return global1
 
+def open_game_club(year):
+    s = requests.get(year)
+    b = bs4.BeautifulSoup(s.text, "html.parser")
+    global1 = b.select('.tab-row-green')
+    return  global1
+
+def music():
+    target = playsound.playsound('file2.mp3', True)
 
 
 #print(sait('анжи', 'авангард'))
 # print(proverka('балтека', 0.7))
 
 '''''
-
+cпартаквладикавказ
 lst = ['авангард', 'уфа', 'анжи', 'ангушт', 'мордовия', 'cпартак-владикавказ', 'рубин', 'камаз', 'нефтехимик', 'ахмат',
        'динамо', 'краснодар', 'сочи', 'черноморец', 'енисей', 'звезда', 'ска-хабаровск', 'салют',
        'торпедо', 'муром', 'ротор-волгоград', 'ротор', 'Факел', 'балтика', 'калуга', 'ленинградец',
